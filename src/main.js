067e268ca5dddcd9965c92ef2a42f3fa3f01e5f3
Zenemig <hola@zenemig.net>
2019-02-15 13:41:27 -0300

    feat(general): import font awesome and normalize.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowCircleLeft } from '@fortawesome/pro-light-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './registerServiceWorker'
+import 'normalize.css/normalize.css'
+
+library.add(faArrowCircleLeft)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
